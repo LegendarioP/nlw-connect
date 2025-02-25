96da44dfcbd570ed290e876f80839dae852036fc
Pablo Luan <pablo-544@hotmail.com>
2025-02-24 21:59:10 -0300

    feat: creating input component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
-import { ArrowRight, Copy } from "lucide-react";
+import { ArrowRight, Copy, User } from "lucide-react";
 import Button from "@/components/button";
 import IconButton from "@/components/icon-button";
+import { InputField, InputIcon, InputRoot } from "@/components/input";
 
 export default function Home() {
   return (
@@ -12,6 +13,17 @@ export default function Home() {
       <IconButton>
         <Copy /> 
       </IconButton>
+
+      {/* <Input placeholder="Digite seu e-mail"/> */}
+      {/* <Input placeholder="Digite seu e-mail" error/> */}
+
+      <InputRoot error>
+        <InputIcon>
+          <User/>
+        </InputIcon>
+
+        <InputField />
+      </InputRoot>
     </main>
   );
 }
