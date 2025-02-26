853e8dc999ce1165a65266f9c5dedf08a547e40e
Pablo Luan <pablo-544@hotmail.com>
2025-02-25 21:18:22 -0300

    feat: create of options under input from page

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -1,12 +1,16 @@
-import { ComponentProps, ReactNode } from "react"
+import type { ComponentProps } from "react"
+import { twMerge } from "tailwind-merge"
 
-interface IconButtonProps extends ComponentProps<'button'> {}
+interface IconButtonProps extends ComponentProps<"button"> {}
 
-export default function IconButton(props: IconButtonProps) {
-    return (
-        <button 
-            className="p-1.5 bg-gray-500 text-blue font-semibold rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900"
-            {...props}
-        />
-    )
+export default function IconButton({ className, ...props }: IconButtonProps) {
+  return (
+    <button
+      className={twMerge(
+        "p-1.5 bg-gray-500 text-blue font-semibold rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900",
+        className
+      )}
+      {...props}
+    />
+  )
 }
