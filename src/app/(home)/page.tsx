6a8b789008d3e61e875fc932b0125de600511364
Pablo Luan <pablo-544@hotmail.com>
2025-02-25 21:47:35 -0300

    chore: updating and optimizing home components

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,9 @@
 import Button from "@/components/button"
+import SubscriptionForm from "@/components/home/subscription-form"
 import { InputField, InputIcon, InputRoot } from "@/components/input"
 import { ArrowRight, Radio, User } from "lucide-react"
 import Image from "next/image"
-import logo from "../assets/logo.svg"
+import logo from "../../assets/logo.svg"
 
 export default function Home() {
   return (
@@ -48,32 +49,7 @@ export default function Home() {
         </div>
 
         {/* FORM */}
-        <form className="bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6 w-full md:max-w-[440px]">
-          <h2 className="font-heading font-semibold text-gray-200 text-xl">
-            Inscrição
-          </h2>
-
-          <div className="space-y-3">
-            <InputRoot>
-              <InputIcon>
-                <User />
-              </InputIcon>
-              <InputField type="text" placeholder="Nome Completo" />
-            </InputRoot>
-
-            <InputRoot>
-              <InputIcon>
-                <User />
-              </InputIcon>
-              <InputField type="text" placeholder="E-mail" />
-            </InputRoot>
-          </div>
-
-          <Button type="submit">
-            Confirmar
-            <ArrowRight />
-          </Button>
-        </form>
+        <SubscriptionForm />
       </div>
     </div>
   )
